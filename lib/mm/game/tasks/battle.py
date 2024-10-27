be8bc31a9b34e99b8009eadbf3e5b1c685beae5d
Brellor / Lorand <brellorand@gmail.com>
2024-10-27 14:32:14 -0400

    refactored battle tasks to genericize the common logic

--- lib/mm/game/tasks/battle.py
+++ lib/mm/game/tasks/battle.py
@@ -5,12 +5,12 @@ Tasks related to main quest and tower battles
 from __future__ import annotations
 
 import logging
-from abc import ABC
+from abc import ABC, abstractmethod
 from functools import cached_property
 from typing import TYPE_CHECKING
 
 from mm.enums import TowerType
-from ..battle import get_available_tower_types
+from ..battle import BattleResultWrapper, get_available_tower_types
 from ..utils import wait
 from .task import Task, TaskConfig
 
@@ -19,6 +19,7 @@ if TYPE_CHECKING:
 
     from mm.game.battle import BattleResult, QuestBattleResult, TowerBattleResult
     from mm.mb_models.quest import Quest
+    from mm.mb_models.tower import TowerBattleQuest
     from mm.typing import UserBattleBossDtoInfo, UserTowerBattleDtoInfo
     from ..session import WorldSession
 
@@ -27,41 +28,135 @@ log = logging.getLogger(__name__)
 
 
 class BattleTask(Task, ABC):
+    battle_type: str
+
+    def __init_subclass__(cls, battle_type: str, **kwargs):  # noqa
+        super().__init_subclass__(**kwargs)
+        cls.battle_type = battle_type
+
     def __init__(
         self,
         world_session: WorldSession,
         config: TaskConfig = None,
         *,
         battle_results_dir: Path | None = None,
+        show_enemies: bool = True,
     ):
         super().__init__(world_session, config)
         self.total = 0
         self.successes = 0
         self.errors = 0
         self.battle_results_dir = battle_results_dir
+        self.show_enemies = show_enemies
+
+    @cached_property
+    def world_player(self) -> str:
+        return f'[W{self.world_session.world_num}:{self.world_session.player_name}]'
 
     def can_perform(self) -> bool:
         return not self.cannot_perform_msg
 
-    def _log_result(self, prefix: str, result: BattleResult, attempts: int, what: str):
+    def perform_task(self):
+        if self.config.dry_run:
+            self._log_battle_start(self._next_battle)
+            return
+
+        while self.can_perform():
+            self._challenge_until_win()
+            self._after_win()
+
+        return self.cannot_perform_msg
+
+    def _after_win(self):
+        wait(self.config)
+
+    def _log_result(self, what: str, result: BattleResult, attempts: int):
         message = (
-            f'{prefix}: {result.result_message}; {what} {attempts=},'
+            f'{self.world_player} Challenged {what}: {result.result_message}; {self.battle_type} {attempts=},'
             f' total={self.total}, successes={self.successes}, errors={self.errors}'
         )
         log.info(message, extra={'color': 10 if result.is_winner else None})
 
+    @property
+    @abstractmethod
+    def _next_battle(self) -> Quest | TowerBattleQuest:
+        raise NotImplementedError
 
-class QuestBattles(BattleTask):
+    @abstractmethod
+    def _get_battle_repr(self, battle: Quest | TowerBattleQuest) -> str:
+        raise NotImplementedError
+
+    def _log_battle_start(self, battle: Quest | TowerBattleQuest):
+        if not self.show_enemies and not self.config.dry_run:
+            return
+
+        if self.config.dry_run:
+            prefix, verb = '[DRY RUN] ', 'Would challenge'
+        else:
+            prefix, verb = '', 'Challenging'
+
+        if self.show_enemies:
+            suffix = f' - enemies: {" ".join(e.get_summary() for e in battle.enemies)}'
+        else:
+            suffix = ''
+
+        log.info(f'{prefix}{self.world_player} {verb} {self._get_battle_repr(battle)}{suffix}')
+
+    @abstractmethod
+    def _challenge_until_win(self):
+        raise NotImplementedError
+
+    def _challenge_battle_until_win(self, battle: Quest | TowerBattleQuest):
+        self._log_battle_start(battle)
+
+        battle_repr = self._get_battle_repr(battle)
+        attempts = 0
+        while True:
+            attempts += 1
+            try:
+                result = self._challenge_once(battle)
+            except Exception as e:
+                log.error(f'{self.world_player} Challenged {battle_repr}; error: {e}', exc_info=True)
+                self.errors += 1
+                if self.errors >= self.config.max_errors:
+                    raise RuntimeError(f'Exceeded allowed error count while challenging {battle_repr}') from e
+            else:
+                self._log_result(battle_repr, result, attempts)
+                if result.is_winner:
+                    return
+
+            wait(self.config)
+
+    @abstractmethod
+    def _challenge_battle(self, battle: Quest | TowerBattleQuest) -> BattleResultWrapper:
+        raise NotImplementedError
+
+    def _challenge_once(self, battle: Quest | TowerBattleQuest) -> BattleResult:
+        self.total += 1
+
+        result_wrapper = self._challenge_battle(battle)
+        if self.battle_results_dir:
+            result_wrapper.save(self.battle_results_dir)
+
+        result = result_wrapper.battle_result
+        if result.is_winner:
+            self.successes += 1
+
+        return result
+
+
+class QuestBattles(BattleTask, battle_type='quest'):
     def __init__(
         self,
         world_session: WorldSession,
         config: TaskConfig = None,
         *,
+        show_enemies: bool = True,
         max_quest: tuple[int, int] = None,
         stop_after: int = None,
         battle_results_dir: Path | None = None,
     ):
-        super().__init__(world_session, config, battle_results_dir=battle_results_dir)
+        super().__init__(world_session, config, battle_results_dir=battle_results_dir, show_enemies=show_enemies)
         self.max_quest = max_quest
         self.stop_after = stop_after
         self._map_info_included_others = False
@@ -77,6 +172,8 @@ class QuestBattles(BattleTask):
     @property
     def _next_quest(self) -> Quest:
         return self.world_session.session.mb.quests[self._last_cleared_id + 1]
+
+    _next_battle = _next_quest
 
     @property
     def cannot_perform_msg(self) -> str | None:
@@ -96,77 +193,44 @@ class QuestBattles(BattleTask):
     def before_run(self):
         self._get_map_info()
 
-    def perform_task(self):
-        if self.config.dry_run:
-            log.info(f'[DRY RUN] Would challenge quest {self._next_quest}')
-            return
-
-        while self.can_perform():
-            self._challenge_until_win()
-            wait(self.config)
-            self._get_map_info()
-            self.world_session.get_next_quest_info()
-            # If the next quest is the first in a chapter, use the between_tasks delay to better simulate needing
-            # to click `Next Chapter` and waiting for the longer animation
-            wait(self.config, between_tasks=self._next_quest.number[1] == 1)
-
-        return self.cannot_perform_msg
+    def _after_win(self):
+        wait(self.config)
+        self._get_map_info()
+        self.world_session.get_next_quest_info()
+        # If the next quest is the first in a chapter, use the between_tasks delay to better simulate needing
+        # to click `Next Chapter` and waiting for the longer animation
+        wait(self.config, between_tasks=self._next_quest.number[1] == 1)
 
     def _get_map_info(self):
         include_others = not self._map_info_included_others
         self.world_session.get_quest_map_info(include_others)
         self._map_info_included_others = include_others
 
+    def _get_battle_repr(self, quest: Quest) -> str:
+        return f'quest {quest}'
+
     def _challenge_until_win(self):
         quest = self._next_quest
         self.world_session.get_quest_info(quest.id)
 
-        log_prefix = f'[W{self.world_session.world_num}:{self.world_session.player_name}] Challenged quest {quest}'
-        attempts = 0
-        while True:
-            attempts += 1
-            try:
-                result = self._challenge_once(quest)
-            except Exception as e:
-                log.error(f'{log_prefix}; error: {e}', exc_info=True)
-                self.errors += 1
-                if self.errors >= self.config.max_errors:
-                    raise RuntimeError(f'Exceeded allowed error count while challenging quest {quest}') from e
-            else:
-                self._log_result(log_prefix, result, attempts, 'quest')
-                if result.is_winner:
-                    return
+        self._challenge_battle_until_win(quest)
 
-            wait(self.config)
-
-    def _challenge_once(self, quest: Quest) -> BattleResult:
-        self.total += 1
-
-        quest_result: QuestBattleResult = self.world_session.battle_quest_boss(quest.id)
-        if self.battle_results_dir:
-            quest_result.save(
-                self.battle_results_dir,
-                f'W{self.world_session.world_num}_{self.world_session.player_name}__quest_{quest}',
-            )
-
-        result = quest_result.battle_result
-        if result.is_winner:
-            self.successes += 1
-
-        return result
+    def _challenge_battle(self, quest: Quest) -> QuestBattleResult:
+        return self.world_session.battle_quest_boss(quest.id)
 
 
-class ClimbTower(BattleTask):
+class ClimbTower(BattleTask, battle_type='floor'):
     def __init__(
         self,
         world_session: WorldSession,
         config: TaskConfig = None,
         *,
+        show_enemies: bool = True,
         tower_type: TowerType = TowerType.Infinite,
         max_floor: int = None,
         battle_results_dir: Path | None = None,
     ):
-        super().__init__(world_session, config, battle_results_dir=battle_results_dir)
+        super().__init__(world_session, config, battle_results_dir=battle_results_dir, show_enemies=show_enemies)
         self.tower_type = tower_type
         self.max_floor = max_floor
 
@@ -176,7 +240,7 @@ class ClimbTower(BattleTask):
 
     @cached_property
     def _absolute_max_floor(self) -> int:
-        return max(f.floor for f in self.world_session.session.mb.tower_type_floors_map[self.tower_type])
+        return max(f.floor for f in self.world_session.session.mb.tower_type_floors_map[self.tower_type].values())
 
     @property
     def tower_info(self) -> UserTowerBattleDtoInfo:
@@ -186,18 +250,20 @@ class ClimbTower(BattleTask):
             raise RuntimeError(f'Could not find tower info for {self.tower_type=}') from e
 
     @property
-    def _current_floor(self) -> int:
-        return self.tower_info['MaxTowerBattleId']
+    def _next_floor_num(self) -> int:
+        return self.tower_info['MaxTowerBattleId'] + 1
 
     @property
-    def _next_floor(self) -> int:
-        return self._current_floor + 1
+    def _next_floor(self) -> TowerBattleQuest:
+        return self.world_session.session.mb.tower_type_floors_map[self.tower_type][self._next_floor_num]
+
+    _next_battle = _next_floor
 
     @property
     def cannot_perform_msg(self) -> str | None:
-        if self.max_floor and self._next_floor > self.max_floor:
+        if self.max_floor and self._next_floor_num > self.max_floor:
             return f'You have reached the configured maximum floor in the {self.tower_type.tower_name}'
-        elif self._next_floor > self._absolute_max_floor:
+        elif self._next_floor_num > self._absolute_max_floor:
             return f'You have reached the maximum available floor in the {self.tower_type.tower_name}'
         elif self.tower_type == TowerType.Infinite:
             return None
@@ -208,61 +274,17 @@ class ClimbTower(BattleTask):
         else:
             return None
 
-    def perform_task(self):
-        if self.config.dry_run:
-            log.info(f'[DRY RUN] Would challenge the {self.tower_type.tower_name} floor {self._next_floor}')
-            return
-
-        while self.can_perform():
-            self._challenge_until_win()
-            wait(self.config)
-
-        return self.cannot_perform_msg
-
-    def _get_log_prefix(self, floor: int) -> str:
+    def _get_battle_repr(self, floor: TowerBattleQuest) -> str:
         if self.tower_type == TowerType.Infinite:
             limit = ''
         else:
             limit = f' [{self.tower_info["TodayClearNewFloorCount"]}/10]'
 
-        ws = self.world_session
-        return f'[W{ws.world_num}:{ws.player_name}] Challenged {self.tower_type.tower_name}{limit} {floor=}'
+        return f'{self.tower_type.tower_name}{limit} floor={floor.floor}'
 
     def _challenge_until_win(self):
         self.world_session.get_tower_reward_info(self.tower_type)
-        floor = self._next_floor
-        attempts = 0
-        while True:
-            attempts += 1
-            try:
-                result = self._challenge_once(floor)
-            except Exception as e:
-                log.error(f'{self._get_log_prefix(floor)}; error: {e}', exc_info=True)
-                self.errors += 1
-                if self.errors >= self.config.max_errors:
-                    raise RuntimeError(
-                        f'Exceeded allowed error count while challenging {self.tower_type.tower_name} {floor=}'
-                    ) from e
-            else:
-                self._log_result(self._get_log_prefix(floor), result, attempts, 'floor')
-                if result.is_winner:
-                    return
+        self._challenge_battle_until_win(self._next_floor)
 
-            wait(self.config)
-
-    def _challenge_once(self, floor: int) -> BattleResult:
-        self.total += 1
-
-        tower_result: TowerBattleResult = self.world_session.start_tower_battle(self.tower_type, floor)
-        if self.battle_results_dir:
-            tower_name = self.tower_type.snake_case
-            tower_result.save(
-                self.battle_results_dir,
-                f'W{self.world_session.world_num}_{self.world_session.player_name}__{tower_name}_{floor}',
-            )
-
-        result = tower_result.battle_result
-        if result.is_winner:
-            self.successes += 1
-
-        return result
+    def _challenge_battle(self, floor: TowerBattleQuest) -> TowerBattleResult:
+        return self.world_session.start_tower_battle(self.tower_type, floor.floor)
